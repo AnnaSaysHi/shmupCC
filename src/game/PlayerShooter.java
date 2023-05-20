f33f4b5e7d76955ff5a9de61a40cd6e7726b30b7
AnnaSaysHi <headsilosetailsyouwin@gmail.com>
2023-05-19 18:24:19 -0700

    added a constructor for this thing

--- src/game/PlayerShooter.java
+++ src/game/PlayerShooter.java
@@ -15,8 +15,23 @@ public class PlayerShooter {
 	int activationFreq;
 	int timer;
 
-	public PlayerShooter() {
+	public PlayerShooter(int activationFreq, int activationDelay, int shotDamage, 
+			double offsetX, double offsetY, double shotAngle, byte optionNum, 
+			int shotGraphic, double shotSize, double hitboxSize,
+			Game game, PlayerShotManager parentManager) {
 		timer = 0;
+		this.activationFreq = activationFreq;
+		this.activationDelay = activationDelay;
+		this.shotDamage = shotDamage;
+		this.offset_x = offsetX;
+		this.offset_y = offsetY;
+		this.shotAngle = shotAngle;
+		this.optionNum = optionNum;
+		this.shotGraphic = shotGraphic;
+		this.shotSize = shotSize;
+		this.hitboxSize = hitboxSize;
+		this.game = game;
+		this.parentManager = parentManager;
 	}
 
 }
