d514a40c7dbe9c13628760117efab61ae2d4007f
AnnaSaysHi <headsilosetailsyouwin@gmail.com>
2023-05-28 19:15:32 -0700

    sounds

--- src/game/SoundManager.java
+++ src/game/SoundManager.java
@@ -1,9 +1,23 @@
 package game;
 
-public class SoundManager {
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineListener;
+
+public class SoundManager implements LineListener {
+	public static final int numSFX = 26;
 
 	public SoundManager() {
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(LineEvent event) {
+        if (LineEvent.Type.START == event.getType()) {
+            System.out.println("Playback started.");
+        } else if (LineEvent.Type.STOP == event.getType()) {
+            System.out.println("Playback completed.");
+        }
+		
 	}
 
 }
