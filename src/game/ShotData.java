e097ab974a4ef69bebc39439576fecc5fec7f9ea
AnnaSaysHi <headsilosetailsyouwin@gmail.com>
2023-08-27 01:17:21 -0700

    update to shot type file format

--- src/game/ShotData.java
+++ src/game/ShotData.java
@@ -5,6 +5,9 @@ import java.io.InputStream;
 import java.nio.ByteBuffer;
 
 public class ShotData {
+	final short shot_reader_version = 1; //for comparison
+	
+	//Header
 	short version_num;
 	short numOffsets;
 	short dmg_cap_type; //for games that intend to support different damage cap implementations per shot type.
@@ -14,12 +17,13 @@ public class ShotData {
 	double itemboxSize;
 	double move_UF;
 	double move_F;
-	short num_power_levels;
 	short option_behavior; //each bit set in this field will enable a special behavior for the shot's options (e.g. tracing, no-render, etc)
+	byte num_power_levels;
 	byte deathbomb_window;
 	byte optionMoveTime;
 	byte num_static_options; // See notes below
-	byte unused1; //purely here to make the header take up a multiple of 64 bits.
+	byte unused1; // Purely here to make the header take up a multiple of 64 bits.
+	byte unused2; // These fields may end up used later on but I couldn't think of a use for them.
 	/* 
 	 * format notes for static options byte
 	 * This byte is of format ANNNNNNN
@@ -34,21 +38,28 @@ public class ShotData {
 	}
 	
 	public void getShotInfoFromFile(String fileName) throws IOException{
-		InputStream shtFile = this.getClass().getResourceAsStream(fileName);
-		readHeader(shtFile);
-		int a = num_power_levels * (num_power_levels + 1) * 2;
-		int b = (num_static_options & 127);
-		b *= 4;
-		numOptionOffsets = (num_static_options == 0) ? a : b;
-		optionPos = new double[numOptionOffsets];
-		readOptionPositions(shtFile);
-		shtFile.close();
+		InputStream shtFile = null;
+		try {			
+			shtFile = this.getClass().getResourceAsStream(fileName);
+			readHeader(shtFile);
+			int a = (int)(num_power_levels) * ((int)(num_power_levels) + 1) * 2;
+			int b = (num_static_options & 127);
+			b *= 4;
+			numOptionOffsets = (num_static_options == 0) ? a : b;
+			optionPos = new double[numOptionOffsets];
+			readOptionPositions(shtFile);			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}finally {
+			shtFile.close();
+		}
 	}
 	private void readHeader(InputStream shtFile) throws IOException {
 		byte[] byteBuf8 = new byte[8];
 		shtFile.readNBytes(byteBuf8, 0, 8);
 		ByteBuffer bb8 = ByteBuffer.wrap(byteBuf8);
 		version_num = bb8.getShort(0);
+		if(version_num != shot_reader_version) throw new IOException (".sht file version does not match reader version " + shot_reader_version);
 		numOffsets = bb8.getShort(2);
 		dmg_cap_type = bb8.getShort(4);
 		dmg_cap_val = bb8.getShort(6);
@@ -69,12 +80,13 @@ public class ShotData {
 		move_F = bb8.getDouble();
 		shtFile.readNBytes(byteBuf8, 0, 8);
 		bb8 = ByteBuffer.wrap(byteBuf8);
-		num_power_levels = bb8.getShort(0);	
-		option_behavior = bb8.getShort(2);
-		deathbomb_window = bb8.get(4);
-		optionMoveTime = bb8.get(5);
-		num_static_options = bb8.get(6);
-		unused1 = bb8.get(7);
+		option_behavior = bb8.getShort(0);
+		num_power_levels = bb8.get(2);
+		deathbomb_window = bb8.get(3);
+		optionMoveTime = bb8.get(4);
+		num_static_options = bb8.get(5);
+		unused1 = bb8.get(6);
+		unused2 = bb8.get(7);
 	}
 	private void readOptionPositions(InputStream shtFile) throws IOException{
 		byte[] byteBuf8 = new byte[8];
