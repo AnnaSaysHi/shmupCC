b1931f4446cc778d562e8619fcddfbe8a3cac189
AnnaSaysHi <headsilosetailsyouwin@gmail.com>
2024-08-07 18:01:12 -0700

    new test script

--- src/game/stages/Script1_6.java
+++ src/game/stages/Script1_6.java
@@ -3,8 +3,13 @@ package game.stages;
 import game.Game;
 import game.StageScript;
 import game.audio.SoundManager;
+import game.bullet.BulletColor;
 import game.bullet.BulletManager;
+import game.bullet.BulletSpawner;
+import game.bullet.BulletTransformation;
+import game.bullet.BulletType;
 import game.enemy.EnemyManager;
+import game.enemy.EnemyMovementInterpolator;
 import game.player.Player;
 
 public class Script1_6 extends StageScript {
@@ -22,9 +27,48 @@ public class Script1_6 extends StageScript {
 
 	@Override
 	public void tick() {
-		// TODO Auto-generated method stub
+		stageTimer++;
+		if(stageTimer % 60 == 0) {
+			boolean toMirror = (stageTimer % 120 == 0);
+			double xSpawn = (parentGame.FetchRNG()).nextDouble();
+			xSpawn = xSpawn * 120;
+			enmMgr.addEnemy(new Enm1(), xSpawn - 60, -32, 300, toMirror);
+		}
 		
 	}
 	
 
+}
+class Enm1 extends game.enemy.Enemy{
+
+	public Enm1() {
+		super();
+	}
+	@Override
+	protected void initActions() {
+		this.setEnemySprite(1);
+		this.setPosRelTime(120, EnemyMovementInterpolator.INTERPOLATION_EASE_IN2, 50, 150);
+		this.setFlag(FLAG_PERSISTENT);
+		//this.setFlag(FLAG_DAMAGE_IMMUNE);
+		this.spawners[0].setRelativePos(0, 0);
+		this.spawners[0].setMode(BulletSpawner.Mode_Fan);
+		this.spawners[0].setSound(SoundManager.EnemyShootMuted);
+		this.spawners[0].setBulletCounts(5, 2);
+		this.spawners[0].setSpeeds(2.5, 1);
+		double ang1 = Math.toRadians(100);
+		double ang2 = Math.toRadians(10);
+		this.spawners[0].setAngles(ang1, ang2);
+		this.spawners[0].setTypeAndColor(BulletType.BALL, BulletColor.BRIGHT_RED);
+		this.spawners[0].setActivationFrequency(50);
+	}
+	@Override
+	protected void doEnemyActions() {
+		if(this.enemyTimer == 160) {
+			this.clearFlag(FLAG_PERSISTENT);
+			this.setPosRelTime(300, EnemyMovementInterpolator.INTERPOLATION_LINEAR, 600, 50);
+		}
+	}
+	
+	
+	
 }
