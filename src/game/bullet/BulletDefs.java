15cc661f297006b6408261148f1b393cc8336655
AnnaSaysHi <headsilosetailsyouwin@gmail.com>
2024-08-19 18:38:23 -0700

    refactored bullet type reference numbers

--- src/game/bullet/BulletDefs.java
+++ src/game/bullet/BulletDefs.java
@@ -4,23 +4,9 @@ package game.bullet;
  * If you want to use a non-default bullet spritesheet, please change the
  * definitions in this class to match with your new bullet spritesheet.
  */
-public abstract class BulletType {
+public abstract class BulletDefs {
 	
 	public static final int NUM_BULLET_TYPES = 11;
-	
-	public static final int LASER_PIECE = 0;
-	public static final int ARROWHEAD = 1;
-	public static final int OUTLINE = 2;
-	public static final int BALL = 3;
-	public static final int RICE = 4;
-	public static final int KUNAI = 5;
-	public static final int SHARD = 6;
-	public static final int AMULET = 7;
-	public static final int BULLET = 8;
-	public static final int BACTERIA = 9;
-	public static final int STAR_CW = 10;
-	public static final int STAR_CCW = 11;
-	
 	
 	public static final int[] BULLET_SPRITESHEET_INDEX = new int[] {
 			0,
